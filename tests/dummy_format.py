502bcfcdf35eeb225fbfa5a27d885034f89bd83e
Jeremy Morse <jeremy.morse@sony.com>
2019-06-12 10:23:14 +0100

    Report dexter-cant-run tests as unsupported

--- tests/dummy_format.py
+++ tests/dummy_format.py
@@ -13,11 +13,15 @@ class DummyFormat(lit.formats.ShTest):
     def execute(self, test, lit_config):
       result = super(DummyFormat, self).execute(test, lit_config)
       tmpDir, tmpBase = TestRunner.getTempPaths(test)
-      with open(tmpDir + '/summary.csv', 'r') as f:
-        lines = f.readlines()
-        reader = csv.reader(lines)
-        rows = [x for x in reader]
-        assert len(rows) == 2
-        metric = float(rows[1][1])
-      result.addMetric('score', lit.Test.RealMetricValue(metric))
+      try:
+        with open(tmpDir + '/summary.csv', 'r') as f:
+          lines = f.readlines()
+          reader = csv.reader(lines)
+          rows = [x for x in reader]
+          assert len(rows) == 2
+          metric = float(rows[1][1])
+          result.addMetric('score', lit.Test.RealMetricValue(metric))
+      except IOError:
+        msg = 'Failed to open summary from {}'.format(tmpDir)
+        result = lit.Test.Result(lit.Test.UNSUPPORTED, msg)
       return result
