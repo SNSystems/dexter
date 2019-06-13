cbd13223a0a32fde5fa292f111a443149ec87007
Tom Weaver <43210711+TomWeaver18@users.noreply.github.com>
2019-06-13 11:41:56 +0100

    reduce indirection in tool and debugger discovery code (#11)

--- dex/debugger/visualstudio/__init__.py
+++ dex/debugger/visualstudio/__init__.py
@@ -20,3 +20,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
+from dex.debugger.visualstudio.VisualStudio2015 import VisualStudio2015
+from dex.debugger.visualstudio.VisualStudio2017 import VisualStudio2017
