2764365c75c96eb3f17acf11454da30aa472eea7
stozer <stephen.tozer@sony.com>
2019-09-04 12:48:18 +0100

    Add remaining dexter-able debuginfo-tests

--- dex/debugger/lldb/LLDB.py
+++ dex/debugger/lldb/LLDB.py
@@ -229,6 +229,7 @@ class LLDB(DebuggerBase):
             "couldn't get the value of variable",
             "couldn't read its memory",
             "couldn't read from memory",
+            "Cannot access memory at address",
         ])
 
         if could_evaluate and not is_irretrievable and not is_optimized_away:
