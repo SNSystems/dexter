a22f993836981567ba1a27e31cf390f320533876
Jeremy Morse <jeremy.morse@sony.com>
2019-10-08 09:30:54 +0100

    Don't try to load dbgeng stuff on Linux
    The name WINFUNCTYPE isn't even available on linux; just avoid loading
    anything at all if we're not on a Windows platform.

--- dex/debugger/dbgeng/__init__.py
+++ dex/debugger/dbgeng/__init__.py
@@ -1,9 +1,12 @@
-from . import breakpoint
-from . import control
 from . import dbgeng
-from . import probe_process
-from . import setup
-from . import symbols
-from . import symgroup
-from . import sysobjs
-from . import utils
+
+import platform
+if platform.system() == 'Windows':
+  from . import breakpoint
+  from . import control
+  from . import probe_process
+  from . import setup
+  from . import symbols
+  from . import symgroup
+  from . import sysobjs
+  from . import utils
